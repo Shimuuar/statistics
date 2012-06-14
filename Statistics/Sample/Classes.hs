4851bc4af5b3c7ec17f678a0514e7dd18a4c92c5
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2012-06-14 22:22:48 +0400

    Rearrange module

--- Statistics/Sample/Classes.hs
+++ Statistics/Sample/Classes.hs
@@ -1,25 +1,26 @@
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE EmptyDataDecls        #-}
 {-# LANGUAGE BangPatterns          #-}
 module Statistics.Sample.Classes (
-    -- * Type class for data samples
-    Sample(..)
-  , accumElements
-  , evalStatistics
-  , estimateWith
-  , E(..)
-    -- * Type class for estimators
-  , FoldEstimator(..)
+    -- * Type classes for estimators
+    FoldEstimator(..)
   , NullEstimator(..)
   , SemigoupEst(..)
-    -- ** Estimators for non-empty samples
+  , Calc(..)
+  , accumElements
+  , evalStatistics
+    -- ** X
+  , E(..)
+  , estimateWith
+  , asEstimator
+    -- ** Non-empty samples
   , NonEmptyEst(..)
   , InitEst(..)
-    -- *
-  , Calc(..)
+    -- * Type class for data samples
+  , Sample(..)
   ) where
 
 
@@ -30,69 +31,6 @@ import qualified Data.Vector.Unboxed   as U
 import qualified Data.Vector.Primitive as P
 import qualified Data.Vector.Storable  as S
 -- import qualified Data.Vector.Generic   as G
-
-----------------------------------------------------------------
--- Sample
-----------------------------------------------------------------
-
--- | Type class for samples. Since statistics doesn't depend on
---   element order sample could be folded in any order.
---
---   It's stripped down version of 'Foldable' but since vectors
---   couldn't be instances of foldable this type class is required.
-class Sample a where
-  -- | Element type of a sample/
-  type Elem a :: *
-  -- | Strict fold over sample.
-  foldSample  :: (acc -> Elem a -> acc) -> acc -> a -> acc
-
-
--- | Add elements from sample to accumulator
-accumElements :: (Sample a, FoldEstimator m (Elem a)) => m -> a -> m
-accumElements = foldSample addElement
-{-# INLINE accumElements #-}
-
--- | Evaluate statistics over sample.
-evalStatistics :: (Sample a, FoldEstimator m (Elem a), NullEstimator m) => a -> m
-evalStatistics = foldSample addElement nullEstimator
-{-# INLINE evalStatistics #-}
-
-estimateWith :: (Sample a, FoldEstimator m (Elem a), NullEstimator m, Calc m r)
-             => E m
-             -> a
-             -> r
-estimateWith est xs = calc $ evalStatistics xs `asEstimator` est
-
-asEstimator :: m -> E m -> m
-asEstimator x _ = x
-
-
--- Instances
-
-instance Sample [a] where
-  type Elem [a] = a
-  foldSample              = foldl'
-  {-# INLINE foldSample  #-}
-
-instance U.Unbox a => Sample (U.Vector a) where
-  type Elem (U.Vector a) = a
-  foldSample             = U.foldl'
-  {-# INLINE foldSample  #-}
-
-instance S.Storable a => Sample (S.Vector a) where
-  type Elem (S.Vector a) = a
-  foldSample             = S.foldl'
-  {-# INLINE foldSample  #-}
-
-instance P.Prim a => Sample (P.Vector a) where
-  type Elem (P.Vector a) = a
-  foldSample             = P.foldl'
-  {-# INLINE foldSample  #-}
-
-instance Sample (V.Vector a) where
-  type Elem (V.Vector a) = a
-  foldSample             = V.foldl'
-  {-# INLINE foldSample  #-}
 
 
 
@@ -121,12 +59,48 @@ class NullEstimator m where
 class SemigoupEst m where
   joinSample :: m -> m -> m
 
-class FoldEstimator m a => NonEmptyEst m a where
-  nonemptyEst :: InitEst a m
+-- | Extract statistics from given estimator
+class Calc m r where
+  calc :: m -> r
+
+-- | Type tag for estimator
+data E m = E
+
+
+-- | Add elements from sample to accumulator
+accumElements :: (Sample a, FoldEstimator m (Elem a)) => m -> a -> m
+accumElements = foldSample addElement
+{-# INLINE accumElements #-}
+
+-- | Evaluate statistics over sample.
+evalStatistics :: (Sample a, FoldEstimator m (Elem a), NullEstimator m) => a -> m
+evalStatistics = foldSample addElement nullEstimator
+{-# INLINE evalStatistics #-}
+
+-- | Estimate some statistics for sample
+estimateWith :: (Sample a, FoldEstimator m (Elem a), NullEstimator m, Calc m r)
+             => E m -- ^ Type parameter for selecting estimator
+             -> a   -- ^ Data sample
+             -> r   -- ^ Statistics to calculate
+estimateWith est xs = calc $ evalStatistics xs `asEstimator` est
+{-# INLINE estimateWith #-}
+
+-- | Select type of estimator. Similar to 'asTypeOf'
+asEstimator :: m -> E m -> m
+asEstimator x _ = x
+
+
+
+----------------------------------------------------------------
+-- Estimator which doesn't work for nonempty samples
+----------------------------------------------------------------
 
 data InitEst a m
   = Init (a -> InitEst a m)
   | Est  m
+
+class FoldEstimator m a => NonEmptyEst m a where
+  nonemptyEst :: InitEst a m
 
 instance FoldEstimator m a => FoldEstimator (InitEst a m) a where
   addElement (Init f) x = f x
@@ -139,17 +113,34 @@ instance SemigoupEst m => SemigoupEst (InitEst a m) where
   joinSample m        (Init f) = Init $ \x -> joinSample (f x) m
   {-# INLINE joinSample #-}
 
-
-----------------------------------------------------------------
-
-class Calc m r where
-  calc :: m -> r
-
-data E m = E
+instance Calc m r => Calc (InitEst a m) (Maybe r) where
+  calc (Init _) = Nothing
+  calc (Est  x) = Just $ calc x
 
 
 
 ----------------------------------------------------------------
+-- Sample
+----------------------------------------------------------------
+
+-- | Type class for samples. Since statistics doesn't depend on
+--   element order sample could be folded in any order.
+--
+--   It's stripped down version of 'Foldable' but since vectors
+--   couldn't be instances of foldable this type class is required.
+class Sample a where
+  -- | Element type of a sample/
+  type Elem a :: *
+  -- | Strict fold over sample.
+  foldSample  :: (acc -> Elem a -> acc) -> acc -> a -> acc
+
+
+
+----------------------------------------------------------------
+-- Instances
+----------------------------------------------------------------
+
+-- Estimators
 
 instance (FoldEstimator a x, FoldEstimator b x) => FoldEstimator (a,b) x where
   addElement (!a,!b) x = (addElement a x, addElement b x)
@@ -162,3 +153,32 @@ instance (NullEstimator a, NullEstimator b) => NullEstimator (a,b) where
 instance (SemigoupEst a, SemigoupEst b) => SemigoupEst (a,b) where
   joinSample (!a1,!b1) (!a2,!b2) = (joinSample a1 a2, joinSample b1 b2)
   {-# INLINE joinSample #-}
+
+
+
+-- Sample
+
+instance Sample [a] where
+  type Elem [a] = a
+  foldSample              = foldl'
+  {-# INLINE foldSample  #-}
+
+instance U.Unbox a => Sample (U.Vector a) where
+  type Elem (U.Vector a) = a
+  foldSample             = U.foldl'
+  {-# INLINE foldSample  #-}
+
+instance S.Storable a => Sample (S.Vector a) where
+  type Elem (S.Vector a) = a
+  foldSample             = S.foldl'
+  {-# INLINE foldSample  #-}
+
+instance P.Prim a => Sample (P.Vector a) where
+  type Elem (P.Vector a) = a
+  foldSample             = P.foldl'
+  {-# INLINE foldSample  #-}
+
+instance Sample (V.Vector a) where
+  type Elem (V.Vector a) = a
+  foldSample             = V.foldl'
+  {-# INLINE foldSample  #-}
