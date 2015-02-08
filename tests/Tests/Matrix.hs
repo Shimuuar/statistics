dc9c963b930fe18d11fbcc961e0ce0c5300efdc1
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2015-02-08 19:07:48 +0300

    Add more tests for QR factorization

--- tests/Tests/Matrix.hs
+++ tests/Tests/Matrix.hs
@@ -29,11 +29,58 @@ t_transpose m = U.concat (map (column n) [0..rows m-1]) === toVector m
 
 -- | Test that A = QR
 t_qr :: Matrix -> Property
-t_qr a = hasNaN p .||. eql 1e-10 a p
+t_qr a
+  = hasNaN p .||.
+    ( counterexample ("Q=\n"++show q)
+    $ counterexample ("R=\n"++show r)
+    $ conjoin [ eql 1e-10 a p
+              , property $ isUpperTri r
+              ]
+    )
   where
     (q,r) = qr a
     p     = q `multiply` r
 
+
+-- | Test that Q is indeed orthogonal
+t_qr_ortho :: Square Matrix -> Property
+t_qr_ortho (Square a)
+  = hasNaN p .||.
+    ( counterexample ("Q=\n"++show q)
+    $ counterexample ("R=\n"++show r)
+    $ conjoin
+      [ counterexample ("I=\n"++show id1) $ isIdentity id1
+      , counterexample ("I=\n"++show id2) $ isIdentity id2
+      ])
+  where
+    (q,r) = qr a
+    p     = q `multiply` r
+    -- Approximately identity matrices
+    id1   = q `multiply` transpose q
+    id2   = transpose q `multiply` q
+
+
+isIdentity :: Matrix -> Bool
+isIdentity m
+  | r /= c    = error "Matrix is mot square!"
+  | otherwise = and [ if i == j then abs (x - 1) < 1e-12 else abs x < 1e-12
+                    | i <- [0 .. r-1]
+                    , j <- [0 .. r-1]
+                    , let x = unsafeIndex m i j
+                    ]
+  where
+    (r,c) = dimension m
+
+isUpperTri :: Matrix -> Bool
+isUpperTri m
+  | r /= c    = error "Matrix is mot square!"
+  | otherwise = and [ x == 0
+                    | i <- [0 .. r-1]
+                    , j <- [0 .. i-1]
+                    , let x = unsafeIndex m i j
+                    ]
+  where
+    (r,c) = dimension m
 
 tests :: Test
 tests = testGroup "Matrix" [
@@ -42,4 +89,5 @@ tests = testGroup "Matrix" [
   , testProperty "t_center" t_center
   , testProperty "t_transpose" t_transpose
   , testProperty "t_qr" t_qr
+  , testProperty "t_qr_ortho" t_qr_ortho
   ]
--- tests/Tests/Matrix/Types.hs
+++ tests/Tests/Matrix/Types.hs
@@ -1,20 +1,29 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Tests.Matrix.Types
     (
       Mat(..)
     , fromMat
-    , toMat
+    -- , toMat
+    , Square(..)
     ) where
 
 import Control.Monad (join)
 import Control.Applicative ((<$>), (<*>))
-import Statistics.Matrix (Matrix(..), fromList, toRowLists)
+import Statistics.Matrix (Matrix(..), fromList)
 import Test.QuickCheck
 import Tests.Helpers (shrinkFixedList, small)
 import qualified Data.Vector.Unboxed as U
+import Debug.Trace
 
+----------------------------------------------------------------
+-- Matrix instances
+----------------------------------------------------------------
+
+-- | Matrix represented as rows
 data Mat a = Mat { mrows :: Int , mcols :: Int
                  , asList :: [[a]] }
               deriving (Eq, Ord, Show, Functor)
@@ -22,8 +31,8 @@ data Mat a = Mat { mrows :: Int , mcols :: Int
 fromMat :: Mat Double -> Matrix
 fromMat (Mat r c xs) = fromList r c (concat xs)
 
-toMat :: Matrix -> Mat Double
-toMat m = Mat (rows m) (cols m) (toRowLists m)
+-- toMat :: Matrix -> Mat Double
+-- toMat m = Mat (rows m) (cols m) (toRowLists m)
 
 
 instance (Arbitrary a) => Arbitrary (Mat a) where
@@ -38,3 +47,24 @@ arbMat (Positive (Small r)) (Positive (Small c)) =
 instance Arbitrary Matrix where
     arbitrary = fromMat <$> arbitrary
     -- shrink    = map fromMat . shrink . toMat
+
+----------------------------------------------------------------
+-- Square matrices
+----------------------------------------------------------------
+
+-- | Square matrix
+newtype Square m = Square { asSquare :: m }
+                   deriving (Show,Functor)
+
+instance Arbitrary a => Arbitrary (Square (Mat a)) where
+  arbitrary = do
+    Positive (Small n) <- arbitrary
+    traceShow n $ return ()
+    xs      <- vectorOf n (vector n)
+    return $ Square $ Mat n n xs
+
+instance Arbitrary (Square Matrix) where
+  arbitrary = do
+    m <- arbitrary
+    traceShow m $ return ()
+    return $ fmap fromMat m
